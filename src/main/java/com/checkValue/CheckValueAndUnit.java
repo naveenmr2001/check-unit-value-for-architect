a7da341a2db8b633ec25c3d72bb8bbedf55bef4b
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-03-30 11:42:48 +0530

    Initial Commit to Project

---
+++ src/main/java/com/checkValue/CheckValueAndUnit.java
@@ -0,0 +1,14 @@
+package com.checkValue;
+
+public class CheckValueAndUnit {
+
+    private int valueOfSize;
+
+    private String unitOfValue;
+
+    public CheckValueAndUnit(int valueOfSize,String unitOfValue){
+        this.valueOfSize = valueOfSize;
+        this.unitOfValue = unitOfValue;
+    }
+
+}
---
+++ src/test/java/com/checkValue/CheckValueForArchitectTest.java
@@ -0,0 +1,21 @@
+package com.checkValue;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+
+public class CheckValueForArchitectTest {
+
+    @Nested
+    class CheckValue{
+
+        CheckValueAndUnit valueOf1Cm = new CheckValueAndUnit(1,"cm");
+
+        @Test
+        void ToCheckIfEqualForInputFor1Cm(){
+
+        }
+
+    }
+
+}
